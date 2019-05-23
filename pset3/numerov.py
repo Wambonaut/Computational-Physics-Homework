21475ac0ea81e7bf370741ad4c61dabe90a3edaa
jona ackerschott <jona.a99@gmail.com>
2019-05-23 09:08:20 +0200

    pset3/numerov.py: Update.

--- pset3/numerov.py
+++ pset3/numerov.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 def numerov(t0, u0, ud0, k, dt, N):
+  """ Numerically solves the ODE u''(t) + k(t) u(t) = 0 using the numerov algorithm """
   t = np.linspace(0, (N - 1) * dt, N)
   u = np.zeros(N)
   u[0] = u0
